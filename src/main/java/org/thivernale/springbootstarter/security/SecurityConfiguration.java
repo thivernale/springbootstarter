f7efa4dfc52c0636674c40502b7d209f49f744e1
thivernale <rainm000n@gmail.com>
2020-04-09 14:17:21 +0300

    Authentication using H2 db and default schema.

--- src/main/java/org/thivernale/springbootstarter/security/SecurityConfiguration.java
+++ src/main/java/org/thivernale/springbootstarter/security/SecurityConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -15,60 +16,72 @@ import org.springframework.security.crypto.password.PasswordEncoder;
  * Tell Spring that this is a web security configuration
  *
  */
+@SuppressWarnings("deprecation")
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-	@Autowired
-	DataSource dataSource;
-	
-	/**
-	 * Configure Authentication mechanism 
-	 * Set your Configuration on the auth object:
-	 */
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		
-		// configure as in-memory authentication with user, password and role
-		auth.inMemoryAuthentication()
-			.withUser("user")
-			.password("pass")
-			.roles("USER")
-			.and()
-			.withUser("foo")
-			.password("bar")
-			.roles("ADMIN")
-			;
-		
-		// configure JDBC authentication
-		auth.jdbcAuthentication()
-			.dataSource(dataSource);
-	}
+    @Autowired
+    DataSource dataSource;
 
-	/**
-	 * Spring Security requires a password encoder
-	 * @return a Spring Bean of type PasswordEncoder
-	 */
-	@Bean
-	@SuppressWarnings(value = { "deprecation" })
-	public PasswordEncoder getPasswordEncoder() {
-		// a password encoder which does nothing
-		// not recommended for real applications, but this is a tutorial
-		return NoOpPasswordEncoder.getInstance();
-	}
-	
-	/**
-	 * Override Authorization configuration
-	 */
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-			//.antMatchers("/**").hasRole("ADMIN")
-			// start from most restrictive rule
-			.antMatchers("/admin").hasRole("ADMIN")
-			.antMatchers("/user").hasAnyRole("ADMIN", "USER")
-			// specify that URLs are allowed by everyone
-			.antMatchers("/"/*, "/static/css", "/static/js"*/).permitAll()
-			.and()
-			.formLogin()
-			;
-	}
+    /**
+     * Configure Authentication mechanism
+     * Set your Configuration on the auth object:
+     */
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        // configure as in-memory authentication with user, password and role
+        /*auth.inMemoryAuthentication()
+            .withUser("user")
+            .password("pass")
+            .roles("USER")
+            .and()
+            .withUser("foo")
+            .password("bar")
+            .roles("ADMIN")
+            ;*/
+
+        // configure JDBC authentication
+        // when the application starts up, these instructions tell Spring
+        // Security to populate the database with certain schema and data
+        auth.jdbcAuthentication()
+        .dataSource(dataSource)
+        .withDefaultSchema()
+        .withUser(
+            User.withUsername("user")
+            .password("pass")
+            .roles("USER"))
+        .withUser(
+            User.withUsername("admin")
+            .password("pass")
+            .roles("ADMIN"));
+    }
+
+    /**
+     * Spring Security requires a password encoder
+     * @return a Spring Bean of type PasswordEncoder
+     */
+    @Bean
+    @SuppressWarnings(value = { "deprecation" })
+    public PasswordEncoder getPasswordEncoder() {
+        // a password encoder which does nothing
+        // not recommended for real applications, but this is a tutorial
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    /**
+     * Override Authorization configuration
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+        //.antMatchers("/**").hasRole("ADMIN")
+        // start from most restrictive rule
+        .antMatchers("/admin").hasRole("ADMIN")
+        .antMatchers("/user").hasAnyRole("ADMIN", "USER")
+        // specify that URLs are allowed by everyone
+        .antMatchers("/"/*, "/static/css", "/static/js"*/).permitAll()
+        .and()
+        .formLogin()
+        ;
+    }
 }

cddafc518e98a9f2fbc41c8a863aab4071a90654
thivernale <rainm000n@gmail.com>
2020-04-15 17:33:26 +0300

    Added filter to intercept Request and set security context based on JWT.

--- src/main/java/org/thivernale/springbootstarter/security/models/CustomUserDetails.java
+++ src/main/java/org/thivernale/springbootstarter/security/models/CustomUserDetails.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 public class CustomUserDetails implements UserDetails {
 
+    private static final long serialVersionUID = 1L;
     private String userName;
     private String password;
     private boolean active;
